7c995e0f4aabe6bf46cdf03ac07b751826a17d90
NazamBun <41399000+NazamBun@users.noreply.github.com>
2026-01-17 02:19:27 +0100

    Fix: restore missing Material3 imports (Button/Card/Checkbox)

--- composeApp/src/commonMain/kotlin/com/nazam/dailytodo/feature/todo/presentation/ui/TodoListScreen.kt
+++ composeApp/src/commonMain/kotlin/com/nazam/dailytodo/feature/todo/presentation/ui/TodoListScreen.kt
@@ -11,6 +11,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
